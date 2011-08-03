462fd0831fd81d6a162af32af631b08b19dd9261
Rodrigo Manhães <rmanhaes@gmail.com>
2011-08-03 20:45:24 -0300

    open house

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+
+gem 'sqlite3'
+
+gem 'inherited_resources', '~>1.2.0'
+gem 'formtastic', '~>1.2.0'
+
+group :test do
+  gem 'rspec-rails', '~>2.6.0'
+  gem 'capybara', '~>1.0.0'
+  gem 'factory_girl_rails', '~>1.1.0'
+  gem 'launchy', '~>2.0.0'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Academico::Application.config.secret_token = '34b881dc4b09186edb2ca1c08ccd4f2839ca964de6bcc1fa4090521941fc00ecd7bb187ce321b17e89a87710a4125ddcbaa3d8738bc416018bb66f954d90cde6'
