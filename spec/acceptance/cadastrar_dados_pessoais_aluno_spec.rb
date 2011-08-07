1d2f3ac78e6762cd796f79757df3fc64a3bec190
Kirill Lassounski <kirillsk8@gmail.com>
2011-08-07 11:21:57 -0300

    teste de aceitacao para formulario incompleto

--- spec/acceptance/cadastrar_dados_pessoais_aluno_spec.rb
+++ spec/acceptance/cadastrar_dados_pessoais_aluno_spec.rb
@@ -30,5 +30,29 @@ feature 'cadastrar dados pessoais do aluno' do
     page.should have_content 'Editar'
     page.should have_content 'Voltar'
   end
+
+  scenario 'cadastro incompleto' do
+    visit new_aluno_path
+    fill_in 'E-mail', :with => 'kirillsk8@gmail.com'
+    fill_in 'Pai', :with => 'Sergio'
+    fill_in 'Mãe', :with => 'Elena'
+    fill_in 'Sexo', :with => 'Masculino'
+    fill_in 'Nacionalidade', :with => 'Brasileiro'
+    fill_in 'Naturalidade', :with => 'Russo'
+    click_button 'Salvar'
+
+
+    page.should have_content 'Nome - deve ser preenchido.'
+    page.should have_content 'Data de nascimento - deve ser preenchida.'
+    page.should have_content 'Estado civil- deve ser preenchido.'
+    page.should have_content 'E-mail: kirillsk8@gmail.com'
+    page.should have_content 'Filiação'
+    page.should have_content 'Pai: Sergio'
+    page.should have_content 'Mãe: Elena'
+    page.should have_content 'Sexo: Masculino'
+    page.should have_content 'Nacionalidade: Brasileiro'
+    page.should have_content 'Naturalidade: Russo'
+    page.should have_content 'Voltar'
+  end
 end
 
