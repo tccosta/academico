60b78d7db5c78e1e4020566201f4d51ccf82361d
Tiago Carvalho <ticarvalhocosta@gmail.com>
2011-08-31 17:14:38 -0300

    Incluindo no model Professor a validação para que todos os campos sejam preenchidos

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,2 +1,9 @@
 class Professor < ActiveRecord::Base
+  validates_presence_of :nome, :msg => "O campo nome deve ser preenchido"
+  validates_presence_of :matricula, :msg => "O campo matricula deve ser preenchido"
+  validates_presence_of :tipo, :msg => "O campo tipo deve ser preenchido"
+  validates_presence_of :area_de_conhecimento, :msg => "O campo Area de conhecimento deve ser preenchido"
+  validates_presence_of :sub_area_de_conhecimento, :msg => "O campo Sub area de conhecimento deve ser preenchido"
+  validates_presence_of :lattes, :msg => "O campo lattes deve ser preenchido"
 end
+
