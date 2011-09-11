c6950bc85c8e3c4369190c063b3b76266a0ab736
João Luiz de Almeida Filho <jlalmeidaf@ig.com.br>
2011-09-11 19:17:39 -0300

    formtastic no aluno

--- app/models/aluno.rb
+++ app/models/aluno.rb
@@ -1,9 +1,22 @@
 class Aluno < ActiveRecord::Base
-  validates :nome,:email,:pai,:mae,:sexo,:estado_civil, :cpf, :rg, :orgao_emissor_do_rg, :estado_do_rg, :presence => {:message => ' - deve ser preenchido.'}
-  validates :data_de_nascimento,:nacionalidade,:naturalidade,:data_de_expedicao_do_rg, :presence => {:message => ' - deve ser preenchida.'}
-  validates :numero_do_titulo_de_eleitor , :estado_do_titulo_de_eleitor, :presence => {:message => ' - deve ser preenchido.'}, :if => :e_necessario_certificado_de_reservista?, :unless => :new_record?
-  validates :zona_do_titulo_de_eleitor, :secao_do_titulo_de_eleitor, :data_de_emissao_do_titulo_de_eleitor, :presence => {:message => ' - deve ser preenchida.'}, :if => :e_necessario_certificado_de_reservista?, :unless => :new_record?
-  validates :numero_do_certificado_de_reservista,:data_de_emissao_do_certificado_de_reservista, :categoria_do_certificado_de_reservista, :orgao_do_certificado_de_reservista, :estado_do_certificado_de_reservista, :presence => {:message => ' - deve ser preenchido' }, :if => :e_necessario_certificado_de_reservista?, :unless => :new_record?
+
+  camposMasc = { :nome => "Nome", :pai => "Pai" ,:mae => "Mae",:sexo => "Sexo",:estado_civil => "Estado Civil", :cpf => "CPF", :rg => "RG", :orgao_emissor_do_rg => "Orgao Emissor do RG", :estado_do_rg => "Estado do RG" }
+  camposMasc.each { |campo| validates campo[0], :presence => { :message => campo[1] + ' - deve ser preenchido.'}}
+  camposFem = {:data_de_nascimento => "Data de Nascimento",:nacionalidade => "Nacionalidade" ,:naturalidade => "Naturalidade",:data_de_expedicao_do_rg => "Data de Expedicao do RG"}
+  camposFem.each { |campo| validates campo[0], :presence => { :message => campo[1] + ' - deve ser preenchida.'}}
+  camposTitulodeEleitor = {:numero_do_titulo_de_eleitor => "Numero do titulo de eleitor", :estado_do_titulo_de_eleitor => "Estado do titulo de eleitor" }
+  camposTitulodeEleitor.each { |campo| validates campo[0], :presence => { :message => campo[1] + ' - deve ser preenchido.'}, :if => :e_necessario_certificado_de_reservista?, :unless => :new_record?}
+
+  camposCertificadodeReservista = { 
+    :numero_do_certificado_de_reservista => "Numero do certificado de reservista", 
+    :data_de_emissao_do_certificado_de_reservista => "Data do certificado de reservista",
+    :categoria_do_certificado_de_reservista => "Categoria do certificado de reservista",
+    :orgao_do_certificado_de_reservista => "Orgao do certificado de reservista",
+    :estado_do_certificado_de_reservista => "Estado do certificado de reservista"
+ }
+  camposCertificadodeReservista.each {|campo| validates campo[0], :presence => { :message => campo[1] + ' - deve ser preenchido.'}, :if => :e_necessario_certificado_de_reservista?, :unless => :new_record?}
+#  validates :zona_do_titulo_de_eleitor, :secao_do_titulo_de_eleitor, :data_de_emissao_do_titulo_de_eleitor, :presence => {:message => ' - deve ser preenchida.'}, :if => :e_necessario_certificado_de_reservista?, :unless => :new_record?
+#validates :numero_do_certificado_de_reservista,:data_de_emissao_do_certificado_de_reservista, :categoria_do_certificado_de_reservista, :orgao_do_certificado_de_reservista, :estado_do_certificado_de_reservista, :presence => {:message => ' - deve ser preenchido' }, :if => :e_necessario_certificado_de_reservista?, :unless => :new_record?
 
   SEXO = ['Masculino','Feminino']
   ESTADO_CIVIL = ['Solteiro(a)','Casado(a)','Separado(a)','Divorciado(a)','Viuvo(a)']
@@ -22,6 +35,6 @@ class Aluno < ActiveRecord::Base
     end
 
   end
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create, :message => 'E-mail - deve ser preenchido.'
 end
 
