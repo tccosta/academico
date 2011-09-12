c2a71c878b49fea20a1294818a8b286ca023d009
João Luiz de Almeida Filho <jlalmeidaf@ig.com.br>
2011-09-12 18:44:15 -0300

    alteracao de dados academicos para exibir mensagem ao entrar com data invalida #3

--- app/models/dados_academico.rb
+++ app/models/dados_academico.rb
@@ -1,3 +1,4 @@
 class DadosAcademico < ActiveRecord::Base
-  validates :matricula, :curso, :semestre, :forma_de_ingresso, :data_da_prova_do_ingresso, :link_do_lattes, :presence => { :message => "- deve ser preenchido(a)"}
+  validates :matricula, :curso, :semestre, :forma_de_ingresso, :link_do_lattes, :presence => { :message => "- deve ser preenchido(a)"}
+  validates :data_da_prova_do_ingresso, :presence => { :message => "- deve ser preenchida ou e invalida" }  
 end
