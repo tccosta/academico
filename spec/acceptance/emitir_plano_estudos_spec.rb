c9ce7419ac6281871a864e180a2273a9b0406952
José Carlos Maciel <juquinhaa@gmail.com>
2011-09-20 18:36:46 -0300

    plano de estudos ok, teste ok

--- spec/acceptance/emitir_plano_estudos_spec.rb
+++ spec/acceptance/emitir_plano_estudos_spec.rb
@@ -3,27 +3,27 @@
 require 'spec_helper'
 
 feature 'emissao de plano de estudos' do
-  # scenario 'tudo ok' do
-  #                   @professor = Factory(:professor, :nome => 'Annabell')
-  #                   @disciplina = Factory(:disciplina, :nome => 'Desenvolvimento Ágil')
-  #                   @turma = Factory(:turma, :semestre_letivo => '2011-2')
-  #                  
-  #                   @aluno = Factory.create(:aluno, :nome => 'Kirill Lassounski')
-  #                  # @turma = @aluno.turmas.create!(@turma)
-  #                  @aluno.turmas << @turma
-  #                 # @turma.alunos << @aluno
-  #                  @turma.save!
-  #                  @aluno.save!
-  #                    
-  #                   puts @turma.alunos
-  #                   puts @aluno.turmas
-  #                   
-  #                   
-  #                   visit plano_estudos_path(@aluno.id)
-  #                 
-  #                   page.should have_content 'plano de Kirill Lassounski'
-  #                  
-  #                 end
+  scenario 'tudo ok' do
+                      @professor = Factory(:professor, :nome => 'Annabell')
+                      @disciplina = Factory(:disciplina, :nome => 'Desenvolvimento Ágil')
+                      @turma = Factory(:turma, :semestre_letivo => '2011-2')
+                     
+                      @aluno = Factory.create(:aluno, :nome => 'Kirill Lassounski')
+                     # @turma = @aluno.turmas.create!(@turma)
+                     @aluno.turmas << @turma
+                    # @turma.alunos << @aluno
+                     @turma.save!
+                     @aluno.save!
+                       
+                      puts @turma.alunos
+                      puts @aluno.turmas
+                      
+                      
+                      visit plano_estudos_aluno_path(@aluno.id)
+                    
+                      page.should have_content 'Nome: Kirill Lassounski'
+                     
+                    end
 
   scenario 'aluno com dados incompletos para emitir plano de estudos' do
     @aluno = Factory(:aluno, :nome => 'Kirill Lassounski')
